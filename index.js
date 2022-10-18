4a1c08604ec8a85e865e837b4c06386be51eed88
Salman Basha <salmanbbasha@gmail.com>
2022-10-19 00:23:28 +0530

    Mobile menu func

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuItem = document.querySelectorAll('.menu-item');
+const dropdown = document.querySelectorAll('.dropdown');
 
 hamburger.addEventListener('click', function () {
   mobileMenu.classList.toggle('disabled');
   hamburger.classList.toggle('showing-close');
+});
+
+function clear(c) {
+  c.forEach(d => {
+    d.classList.remove('active');
+  });
+}
+
+function updateItem(item, id) {
+  for (let i = 0; i < 3; i++) {
+    if (Number(id) != i) {
+      item[i].classList.remove('active');
+    }
+  }
+}
+
+menuItem.forEach(item => {
+  item.addEventListener('click', function () {
+    item.classList.add('active');
+    clear(dropdown);
+    dropdown[item.dataset.id].classList.toggle('active');
+    updateItem(menuItem, item.dataset.id);
+  });
 });
